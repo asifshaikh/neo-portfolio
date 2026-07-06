59f7b74315261a0426cf0cde043d5f2465eb5c3b
asifshaikh <shaikha015@gmail.com>
2026-07-06 22:33:46 +0530

    changed bg color and added opensource constants

---
+++ src/data/opensource.ts
@@ -0,0 +1,14 @@
+const openSourceProjects: {
+  name: string
+  url: string
+}[] = [
+  {
+    name: 'Project 1',
+    url: 'https://github.com/user/project1',
+  },
+  {
+    name: 'Project 2',
+    url: 'https://github.com/user/project2',
+  },
+]
+export default openSourceProjects
